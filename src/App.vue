610d3e61e95fcd837ca956605e02754fadc41d81
rohitdas13595 <rohitdas10595@gmail.com>
2022-06-14 18:22:28 +0530

    first

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <router-view name="NavBar"></router-view>
+    <router-view name="StudioNavBar"></router-view>
+    <v-content
+      :class="{
+        'content-bg': ![
+          'SignIn',
+          'SignUp',
+          'Dashboard',
+          'Video',
+          'Detail'
+        ].includes(this.$route.name)
+          ? true
+          : false
+      }"
+    >
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+.content-bg {
+  background-color: #a4d7d4;
+  
+}
+.card {
+  background: #f9f9f9 !important;
+}
+</style>
